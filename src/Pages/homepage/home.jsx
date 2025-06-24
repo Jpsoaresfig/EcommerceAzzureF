ae212c92a8ad95726b9d24b6c676568f730e094c
Jpsoaresfig <jpbusinesss@hotmail.com>
2025-06-24 16:33:50 -0300

    ajust homepage

--- src/Pages/homepage/home.jsx
+++ src/Pages/homepage/home.jsx
@@ -24,23 +24,33 @@ function Home() {
       </section>
 
       {/* Destaques visuais de produtos */}
-      <section className="banner">
-          <h2 className="section-title">Destaques da Semana</h2>
-          <div className="banner-images">
+       <section className="banner">
+        <h2 className="section-title">Destaques da Semana</h2>
+          <div className="banner-carousel">
             <Carousel />
           </div>
-        
-      </section>
 
+          <div className="azzure-card">
+            <h3 className="azzure-title">Azzure</h3>
+            <p>Na Azzure, oferecemos praticidade pra você:</p>
+            <ul>
+              <li><strong>Retirada:</strong> em pontos específicos é só nos chamar pra saber os locais disponíveis.</li>
+              <li><strong>Entrega:</strong> via 99 Flash ou Uber Flash.</li>
+            </ul>
+            <p>Faça seu pedido agora mesmo e aproveite a liberdade de escolher como quer receber seus produtos!</p>
+          </div>
+        
+       
+      </section>
       {/* Promoções */}
       <section className="promotions">
         <h2 className="section-title">Ofertas Imperdíveis</h2>
         <p className="text-center">Descontos especiais para membros da nossa comunidade</p>
       </section>
 
-     
 
-     
+
+
     </div>
   );
 }
