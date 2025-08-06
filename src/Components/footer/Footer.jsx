554de7dbd4122d77ee1eb08811fe7d20b476407a
Jpsoaresfig <jpbusinesss@hotmail.com>
2025-08-06 19:04:49 -0300

    ajust layout

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 LojePay - Todos os direitos reservados</p>
+      <p className="footer-text">© 2025 LojePay - Todos os direitos reservados</p>
     </footer>
   );
 }
